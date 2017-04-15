d290ee07b3457392937dc49498bfca9c42fefc6f
srini <srinivas.shikar@gmail.com>
2017-04-15 12:04:00 +0530

    Initial commit

---
+++ app/src/main/java/com/srini/admin/atpwelcomesliderapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.srini.admin.atpwelcomesliderapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
